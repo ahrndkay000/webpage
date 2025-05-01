4dd74866cf89a501792c1b4da66156db1a90ea9c
ahrndkay000 <127278796+ahrndkay000@users.noreply.github.com>
2025-05-01 07:32:37 -0500

    Update script.js
    Add more metadata to recipe card

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     <h2>${recipe["RECIPE NAME"]}</h2>
                     <img src="${recipe["IMAGE"]}" alt="${recipe["RECIPE NAME"]}" width="200">
                     <p><strong>Meal Type:</strong> ${recipe["MEAL TYPE"]}</p>
+                    <p><strong>Primary Nutrients:</strong> ${recipe['PRIMARY NUTRIENTS']}</p>
+                    <p><strong>Cost Estimate:</strong> ${recipe['COST ESTIMATE ($-$$$)']}</p>
                     <p><a href="recipe-template.html?name=${encodeURIComponent(recipe["RECIPE NAME"])}">View Recipe</a></p>
                 `;
                 container.appendChild(div);
+                
             });
         })
         .catch(error => console.error("Error loading JSON:", error));
